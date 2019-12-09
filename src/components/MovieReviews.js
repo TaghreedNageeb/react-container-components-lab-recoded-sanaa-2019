bf8f77c0ef35504c945ed69a7d97b1927ae4df04
Taghreed Nageeb <taghreed.n.d2018@gmail.com>
2019-12-09 17:15:30 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,19 +1,18 @@
 // Code MovieReviews Here
-import React from 'react';
-
-const MovieReviews = ({reviews}) => 
-<div  className="review-list">
-{reviews.map(review => 
-    {
-    return(
-        <div className="review">
+import React from 'react'
+const MovieReviews=({reviews})=>(
+    <div className="review-list">{reviews.map(review=>
+        {return(
+        <div className="review-list">
         <h1 className="review">{review.display_title}</h1>
         <img src={review.multimedia.src} alt=""/>
-        <p>{review.summary_short}</p>
+        <h2>{review.summary_short}</h2>
+        <a href={review.link.url}> run</a>
         </div>
-    )
-})
-}
-</div>
-
-export default MovieReviews;
+    )})}
+    </div>
+)
+export default MovieReviews 
+MovieReviews.defaultProps = {
+    reviews: []
+  };
